f3601586084207f0e706395973ec6818aef127fd
kath4pk <k.jimenez.molina200@gmail.com>
2024-10-03 10:29:36 -0300

    Clase libreria

---
+++ app/src/main/java/librosfera/modelo/Libreria.java
@@ -0,0 +1,22 @@
+
+public class Libreria {
+
+
+    private String lista_lectura;
+    private String historial_lectura;
+    private int libros_devueltos;
+    private int libros_arrendados;
+    private String resenas_escritas;
+
+    public Libreria(String lista_lectura, String historial_lectura, int libros_devueltos, int libros_arrendados, String resenas_escritas){
+        this.lista_lectura;
+        this.historial_lectura;
+        this.libros_devueltos;
+        this.libros_arrendados;
+        this.resenas_escritas;
+    }//constructor vacio
+
+
+
+
+}//final clase 
